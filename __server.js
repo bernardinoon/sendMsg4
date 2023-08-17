db834099a527db0425a7d50259e6b15a23d95784
bernardinoon <142241467+bernardinoon@users.noreply.github.com>
2023-08-17 17:42:43 -0300

    Update __server.js

--- __server.js
+++ __server.js
@@ -3,7 +3,7 @@ const express = require('express');
 //const cors = require('cors');
 const app = express();
 const path = require('path');
-const port = 3001;
+const port = 8080;
 
 
 // Configura o servidor para lidar com requisições POST
